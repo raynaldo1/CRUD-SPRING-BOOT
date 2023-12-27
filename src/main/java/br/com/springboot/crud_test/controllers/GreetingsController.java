76827c56f83d7f8e6021f47875253bf37363bbc8
Raynaldo Macedo <raynaldo.macedo93@gmail.com>
2023-12-27 09:40:04 -0300

    Removed unused libraries

--- src/main/java/br/com/springboot/crud_test/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/crud_test/controllers/GreetingsController.java
@@ -6,20 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springboot.crud_test.model.UserClass;
 import br.com.springboot.crud_test.repository.UserRepository;
 
-/**
- *
- * A sample greetings controller to return greeting text
- */
 @RestController
 public class GreetingsController {
 
