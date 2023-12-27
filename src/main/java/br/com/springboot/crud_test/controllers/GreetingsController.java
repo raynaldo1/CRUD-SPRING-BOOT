80ae21b2c9aea07b7310474ae26d35c34fa97e92
Raynaldo Macedo <raynaldo.macedo93@gmail.com>
2023-12-27 16:51:34 -0300

    UPDATE method

--- src/main/java/br/com/springboot/crud_test/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/crud_test/controllers/GreetingsController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -57,5 +58,15 @@ public class GreetingsController {
         UserClass user = userRepository.findById(iduser).get();
 
         return new ResponseEntity<UserClass>(user, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "update")
+    @ResponseBody
+    public ResponseEntity<UserClass> update(@RequestBody UserClass user) {
+        
+        UserClass updatedUser = userRepository.saveAndFlush(user);
+
+        return new ResponseEntity<UserClass>(updatedUser, HttpStatus.OK);
+
     }
 }
